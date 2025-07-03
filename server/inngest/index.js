90b1b27117f0e89b775fb456b599dad0270e401f
omnigod-web <nishant14022004@gmail.com>
2025-07-03 09:49:32 +0530

    typo error fixed in index.js

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -8,7 +8,7 @@ export const inngest = new Inngest({ id: "movie-ticket-booking" });
 
 const syncUserCreation=inngest.createFunction(
     {id:'sync-user-from-cleark'},
-    {event:'clear/user.created'},
+    {event:'clerk/user.created'},
     async({event})=>{
         const{id,first_name , last_name , email_addresses ,image_url} = event.data
         const userData={
@@ -25,7 +25,7 @@ const syncUserCreation=inngest.createFunction(
 
 const syncUserDeletion=inngest.createFunction(
     {id:'delete-user-with-clerk'},
-    {event:'clear/user.deleted'},
+    {event:'clerk/user.deleted'},
     async({event})=>{
        const{id}=event.data
        await User.findByIdAndDelete(id)
@@ -36,7 +36,7 @@ const syncUserDeletion=inngest.createFunction(
 
 const syncUserUpadation=inngest.createFunction(
     {id:'update-user-from-cleark'},
-    {event:'clear/user.updated'},
+    {event:'clerk/user.updated'},
     async({event})=>{
          const{id,first_name , last_name , email_addresses ,image_url} = event.data
 
