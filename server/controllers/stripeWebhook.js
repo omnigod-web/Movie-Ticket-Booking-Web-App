3b118294874f07b273fc10d1af00e8206d2746c3
omnoigod1 <your.email@example.com>
2025-12-07 11:55:48 +0530

    beutified code in stripr webhook

--- server/controllers/stripeWebhook.js
+++ server/controllers/stripeWebhook.js
@@ -1,7 +1,7 @@
 import stripe from "stripe";
 import Booking from "../models/Booking.js";
 
-export const stripeWebhooks =async (req , res)=>{
+export const stripeWebhooks = async (req , res)=>{
     const stripeInstance = new stripe(process.env.STRIPE_SECRET_KEY);
     const sig =req.headers['stripe-signature'];
     console.log('webhook is running ');
@@ -20,7 +20,7 @@ export const stripeWebhooks =async (req , res)=>{
                 const sessionList = await stripeInstance.checkout.sessions.list({
                     payment_intent:paymentIntent.id
                 })
-                const session = session.data[0];
+                const session = sessionList.data[0];
                 const {bookingId} = session.metadata;
                 await Booking.findByIdAndUpdate(bookingId ,{
                     isPaid :true,
