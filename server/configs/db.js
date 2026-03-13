8030a3e9413a87731e5f8601ff43951ef228a923
omnoigod1 <your.email@example.com>
2026-03-13 08:12:17 +0530

    requiest forword to live server

--- server/configs/db.js
+++ server/configs/db.js
@@ -4,6 +4,7 @@ const connectDB =async ()=>{
     try {
         mongoose.connection.on('connected' , ()=> console.log('db connected'))
         await mongoose.connect(`${process.env.MONGODB_URI}/quickshow`)
+         console.log("Connected DB:", mongoose.connection.name);
     } catch (error) {
          console.log(error.message)
     }
